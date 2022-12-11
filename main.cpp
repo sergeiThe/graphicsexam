caab9e1b20290ff6c66ae263c66c465761a9dbe3
Sergei <sergeitheprogrammer@gmail.com>
2022-12-11 20:02:30 +0100

    Add glad

--- main.cpp
+++ main.cpp
@@ -1,32 +1,39 @@
-#include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() 
+#include <iostream>
+
+int main()
 {
-    GLFWwindow* window;
+    GLFWwindow *window;
 
-   /* Init GLFW */
-    if( !glfwInit() )
-        exit( EXIT_FAILURE );
+    /* Init GLFW */
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
 
-    window = glfwCreateWindow( 400, 400, "Boing (classic Amiga demo)", NULL, NULL );
+    window = glfwCreateWindow(800, 600, "Boing (classic Amiga demo)", NULL, NULL);
     if (!window)
     {
         printf("Window failed\n");
         glfwTerminate();
-        exit( EXIT_FAILURE );
+        exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
     for (;;)
     {
-       /* Swap buffers */
+        /* Swap buffers */
         glfwSwapBuffers(window);
         glfwPollEvents();
 
-       /* Check if we are still running */
+        /* Check if we are still running */
         if (glfwWindowShouldClose(window))
             break;
-    } 
+    }
     glfwTerminate();
     std::cout << "Hello\n";
     return 0;
